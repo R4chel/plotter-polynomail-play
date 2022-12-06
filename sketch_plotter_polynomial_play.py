f80411ae8b0c61648f9c736d98bfc375c6fc7c9d
R4chel <rae84@cornell.edu>
2022-12-06 16:28:26 -0500

    mid nonsense

--- sketch_plotter_polynomial_play.py
+++ sketch_plotter_polynomial_play.py
@@ -12,7 +12,7 @@ class PlotterPolynomialPlaySketch(vsketch.SketchClass):
     max_delta = vsketch.Param(0.1)
     precision = vsketch.Param(3)
     debug = vsketch.Param(False)
-    mode = vsketch.Param("linear", choices=vsketch.EASING_FUNCTIONS.keys())
+    # mode = vsketch.Param("linear", choices=vsketch.EASING_FUNCTIONS.keys())
     y_offset = vsketch.Param(0.3, decimals=2)
     y_delta = vsketch.Param(-0.01, decimals=6)
 
@@ -57,19 +57,21 @@ class PlotterPolynomialPlaySketch(vsketch.SketchClass):
 
         
         (xs, ys) = f.linspace(1000)
-        ys = ys + self.y_offset
-        pts = LineString(list(zip(xs, ys)))
+        ys_to_draw = ys + self.y_offset
+        pts = LineString(list(zip(xs, ys_to_draw)))
         pts = pts.intersection(self.region)
         vsk.geometry(pts)
         for i in range(self.numLines-1):
-            zs = np.full(len(xs), i)
+            # zs = np.full(len(xs), i)
+            zs = [ i*np.sin(j) for j in range(len(xs))]
             noise = vsk.noise(xs,ys,zs, grid_mode=False)
-            # scaled_noise = list(map(lambda x: vsk.map(x,0,1,-self.max_delta, self.max_delta), noise))
-
             # noise = np.sqrt(noise)
-            scaled_noise = vsk.easing(noise,mode=self.mode, start1= 0, stop1=1, start2=-self.max_delta, stop2=self.max_delta, )
+            noise = noise ** 2
+            scaled_noise = list(map(lambda x: vsk.map(x,0,1,-self.max_delta, self.max_delta), noise))
+
+            # scaled_noise = vsk.easing(noise,mode=self.mode, start1= 0, stop1=1, start2=-self.max_delta, stop2=self.max_delta, )
+            # scaled_noise = vsk.easing(noise,mode=self.mode, start1= 0, stop1=1, start2=0, stop2=self.max_delta, )
             ys = np.add(ys,scaled_noise)
-            # ys = ys + self.y_delta
             ys_to_draw = ys + self.y_delta * i + self.y_offset
             pts = LineString(list(zip(xs, ys_to_draw)))
             pts = pts.intersection(self.region)
