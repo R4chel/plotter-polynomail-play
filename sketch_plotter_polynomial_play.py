e0d12ea62d46b1b6a9d1d0520339977e7b960128
R4chel <rae84@cornell.edu>
2022-12-29 14:34:33 -0500

    reorder operations to improve determinsim (I don't want number of layers to affect the polynomial)

--- sketch_plotter_polynomial_play.py
+++ sketch_plotter_polynomial_play.py
@@ -5,6 +5,7 @@ from numpy.polynomial import Polynomial
 from shapely.geometry import Point, LineString, Polygon
 from shapely.ops import clip_by_rect
 import math
+import functools
 
 fns = {
     "zero": (lambda x: x - x),
@@ -30,12 +31,14 @@ class PlotterPolynomialPlaySketch(vsketch.SketchClass):
     y_delta = vsketch.Param(0.01, decimals=6)
     max_delta = vsketch.Param(0.1)
     draw_first_line = vsketch.Param(False)
+
     layer_count = vsketch.Param(1)
+    random_layer_order = vsketch.Param(False)
 
     def draw_polynomial(self, vsk, coefficients, i):
         f = Polynomial(coefficients)
         (xs, ys) = f.linspace(self.num_pts)
-        ys_to_draw = ys + (self.y_offset * i)
+        ys_to_draw = ys + self.y_offset * (i * self.y_delta)
         pts = LineString(list(zip(xs, ys_to_draw)))
         pts = pts.intersection(self.region)
         vsk.geometry(pts)
@@ -75,14 +78,20 @@ class PlotterPolynomialPlaySketch(vsketch.SketchClass):
             vsk.line(0, 0, 0, y_max)
             vsk.line(0, 0, x_max, 0)
 
-        self.draw_polynomial(vsk, coefficients, 0)
-        layers = range(1, self.layer_count + 1)
-        for i in range(1, self.numLines + 1):
+        lines = [coefficients]
+        for i in range(1, self.numLines):
             coefficients = [
                 c + round(vsk.random(-self.max_delta, self.max_delta),
                           self.precision) for c in coefficients
             ]
-            self.draw_polynomial(vsk, coefficients, i)
+            lines.append(coefficients)
+
+        layers = range(1, self.layer_count + 1)
+        for i in range(len(lines)):
+            layer = layers[math.floor(vsk.random(0, len(layers))) if self.
+                           random_layer_order else i % len(layers)]
+            vsk.stroke(layer)
+            self.draw_polynomial(vsk, lines[i], i)
 
     def finalize(self, vsk: vsketch.Vsketch) -> None:
         vsk.vpype("linemerge linesimplify reloop linesort")
